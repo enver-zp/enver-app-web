04d9151fdbb2167dff346716787b78eff95c0441
Enver <enver.4424@gmail.com>
2026-06-14 20:48:45 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -11,12 +11,12 @@ import {
 } from 'lucide-react';
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
+  apiKey: "AIzaSyANKRyxoEH4MGfko769FLjjNZZzUpg1yx4",
+  authDomain: "proje-f8ab3.firebaseapp.com",
+  projectId: "proje-f8ab3",
+  storageBucket: "proje-f8ab3.firebasestorage.app",
+  messagingSenderId: "655666500599",
+  appId: "1:655666500599:web:43ff5f8003299d26efd4f2"
 };
 
 const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
